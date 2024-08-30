4a9c7804a6b32a12b6c12a112d06ae656a057119
Kenton <kentontang0202@gmail.com>
2024-08-29 23:37:34 -0400

    Initial React portfolio commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Portfolio</h1>
+        <p>Hello! I'm [Your Name], a software developer.</p>
+      </header>
+      <section>
+        <h2>Projects</h2>
+        <ul>
+          <li>Project 1: Description</li>
+          <li>Project 2: Description</li>
+          {/* Add more projects */}
+        </ul>
+      </section>
+      <footer>
+        <p>Contact: [Your Email]</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
